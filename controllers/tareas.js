ebff1cd9a759db884044779cd6f8507b03824703
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-04-27 00:35:56 -0500

    avance semana05

--- controllers/tareas.js
+++ controllers/tareas.js
@@ -1,4 +1,13 @@
-const tareas = [];
+const tareas = [
+    {
+        nombre:"Hacer el foro",
+        estado:true,
+    },
+    {
+        nombre:"Hacer el ejercicio de Flask",
+        estado:false,
+    }
+];
 
 export const crearTarea=(req,res)=>{
     console.log(req.body);
@@ -7,3 +16,23 @@ export const crearTarea=(req,res)=>{
         content:tareas[tareas.length-1],
     });
 };
+
+export const listarTareas=(req,res)=>{
+    return res.json({
+        content:tareas,
+    });
+};
+
+export const listarTareaPorId=(req,res)=>{
+    console.log(req.params);
+    const {id}=req.params;
+    if(tareas[id-1]){
+        return res.json({
+            content:tareas[id-1]
+        })
+    }else{
+        return res.status(404).json({
+            message:"Not found",
+        });
+    }
+}
--- routes/tareas.js
+++ routes/tareas.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import {crearTarea} from "../controllers/tareas"
+import {crearTarea, listarTareaPorId, listarTareas} from "../controllers/tareas"
 
 export const tareas_router=Router();
 
-tareas_router.route("/tareas").post(crearTarea);
+tareas_router.route("/tareas").post(crearTarea).get(listarTareas);
+tareas_router.route("/tareas/:id").get(listarTareaPorId);
