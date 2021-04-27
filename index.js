0e0b0d13ceb7cd9009d043d0a83fe5c4388ec595
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-04-27 12:40:09 -0500

    subir

--- index.js
+++
@@ -1,8 +0,0 @@
-//? Con js puro 
-// const {Server}=require("./config/Server");
-//? Con babelrc
-import {Server} from "./config/Server"
-
-const objServer = new Server();
-objServer.iniciarServidor();
-// console.log("prueba");
--- routes/tareas.js
+++
@@ -1,7 +0,0 @@
-import {Router} from "express";
-import {crearTarea, listarTareaPorId, listarTareas} from "../controllers/tareas"
-
-export const tareas_router=Router();
-
-tareas_router.route("/tareas").post(crearTarea).get(listarTareas);
-tareas_router.route("/tareas/:id").get(listarTareaPorId);
