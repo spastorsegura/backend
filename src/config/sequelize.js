54388cb9358f359ff118fb08408b8cf67f8955e1
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-04-27 23:04:46 -0500

    conexion con la bd

---
+++ src/config/sequelize.js
@@ -0,0 +1,15 @@
+import {Sequelize} from "sequelize";
+
+export const conexion=new Sequelize(
+    "almacen",
+    "root",
+    "Anotherpl@ce1",
+    {
+        dialect:"mysql",
+        host:"127.0.0.1",
+        port:3306,
+        timezone:"-05:00",//no funciona en SQLITE
+        dialectOptions:{
+            dateString:true,
+        }
+    })
--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import {json} from "body-parser";
+import {conexion} from "./sequelize"
 
 export default class Server{
     constructor(){
@@ -11,8 +12,15 @@ export default class Server{
         this.app.use(json());
     }
     start(){
-        this.app.listen(this.port,()=>{
+        this.app.listen(this.port,async()=>{
             console.log(`Servidor corriendo en: http://127.0.0.1:${this.port}`);
+            try {
+                //no usar force(borra y vuelve a cargar), sino alter(solo carga cambios)
+                await conexion.sync();
+                console.log('Base de datos sincronizada correctamente');
+            } catch (error) {
+                console.error(error);
+            }
         });
     }
 }
